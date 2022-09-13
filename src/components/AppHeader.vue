d82b7334c193e4bbbc00bca4c612593cbb594636
TranTam <tamvaa1306@gmail.com>
2022-09-13 08:04:12 +0700

    proptype

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-header">
+    <h1>This is header</h1>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    msg: {
+      required: true,
+      type: String as PropType<string>,
+    },
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="home">
+    <app-header :msg="msg"></app-header>
     <h1>This is home page</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import AppHeader from "@/components/AppHeader.vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
+  components: { AppHeader },
   name: "HomeView",
+  setup() {
+    const msg = ref<string>("Hello this is Vue3 + TypeScript");
+
+    return { msg };
+  },
 });
 </script>
